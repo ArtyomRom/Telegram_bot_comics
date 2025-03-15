2acf87492396383a56ebfc5ba47d5329d906e181
Artyom <zik21b@gmail.com>
2025-03-15 12:33:43 +0300

    Перенес настройки логера и исключения

--- bot.py
+++ bot.py
@@ -5,17 +5,12 @@ import os
 import logging
 
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-logger = logging.getLogger(__name__)
+
 
 
 def send_photo(image_comic, comment, chat_id, token):
     bot = Bot(token=token)
-    try:
-        bot.send_photo(chat_id=chat_id, photo=image_comic, caption=comment)
-        logger.info("✅ Фото отправлено успешно!")
-    except Exception as e:
-        logger.error(f"❌ Ошибка отправки фото: {e}")
+    bot.send_photo(chat_id=chat_id, photo=image_comic, caption=comment)
 
 def main():
     load_dotenv()
@@ -25,8 +20,14 @@ def main():
     comic = get_comic()
     image_comic = comic['img']
     comment = comic['alt']
-    send_photo(image_comic, comment, chat_id, token)
+    try:
+        send_photo(image_comic, comment, chat_id, token)
+        logger.info("✅ Фото отправлено успешно!")
+    except Exception as e:
+        logger.error(f"❌ Ошибка отправки фото: {e}")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    logger = logging.getLogger(__name__)
     main()
